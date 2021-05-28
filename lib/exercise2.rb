571b7b06a74b797fc15de55552cf595c55d8e735
Cynthia Fu <yinnycfu@gmail.com>
2021-05-28 11:54:00 +0100

    identified TypeErrror in encode and corrected

--- lib/exercise2.rb
+++ lib/exercise2.rb
@@ -1,13 +1,14 @@
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
+    p cipher.find_index(char)
     (65 + cipher.find_index(char)).chr
   end
   ciphertext_chars.join
 end
 
 def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
     cipher[65 - char.ord]
   end
