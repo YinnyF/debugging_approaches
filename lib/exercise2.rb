0bae66c511e2dce40291aa8dd2d87751b253e408
Cynthia Fu <yinnycfu@gmail.com>
2021-05-28 12:02:08 +0100

    resolved bugs in exercise 2. Got expected answers

--- lib/exercise2.rb
+++ lib/exercise2.rb
@@ -1,7 +1,6 @@
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
-    p cipher.find_index(char)
     (65 + cipher.find_index(char)).chr
   end
   ciphertext_chars.join
@@ -10,7 +9,7 @@ end
 def decode(ciphertext, key)
   cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
+    cipher[char.ord - 65]
   end
   plaintext_chars.join
 end
